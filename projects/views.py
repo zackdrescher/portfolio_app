97914c812595a4b6ccae8e8871557c2ab020afaf
Zack Drescher <zackdrescher@gmail.com>
2019-04-14 16:09:29 -0400

    cerate project index veiw

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+    """Index page containing all projects"""
+    # query all projects 
+    projects = Project.objects.all()
+    # build the context
+    context = {
+        'projects': projects
+    }
+    # render the html with the given context
+    return render(request, 'project_index.html', context)
