bb268ae66d105a75e07bf9532b0c3c1b179204ae
Zack Drescher <zackdrescher@gmail.com>
2019-04-14 16:12:21 -0400

    create project detail veiw

--- projects/views.py
+++ projects/views.py
@@ -11,3 +11,12 @@ def project_index(request):
     }
     # render the html with the given context
     return render(request, 'project_index.html', context)
+
+def project_detail(request, pk):
+    """detail for project pk"""
+    # retreive project
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
