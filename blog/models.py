88e319cec2859eb922e3cac179c49721381853de
Zack Drescher <zackdrescher@gmail.com>
2019-04-14 17:18:05 -0400

    bug fix

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_len=20)
+    name = models.CharField(max_length=20)
 
 class Post(models.Model):
-    title = models.CharField(max_lenght=255 )
+    title = models.CharField(max_length=255 )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
