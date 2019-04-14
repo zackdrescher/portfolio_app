b9bb61877a88cd2ce85841c0300903bbf26e2840
Zack Drescher <zackdrescher@gmail.com>
2019-04-14 17:55:18 -0400

    create comment form and add it to blog detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
+from . import CommentForm
+
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
@@ -7,6 +9,7 @@ def blog_index(request):
         'posts': posts,
     }
     return render(request, 'blog_index.html', context)
+
 
 def blog_categoty(request, category):
     posts = Post.objects.filter(
@@ -20,11 +23,27 @@ def blog_categoty(request, category):
     }
     return render(request, "blog_category.html", context)
 
+
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    # check for comment posts
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            # Add the comment to db
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         'post': post,
         'comments': comments,
+        'form': CommentForm(),
     }
     return render(request, "blog_detail.html", context)
